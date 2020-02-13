67e10155b460e2c5dc87c3dbd31becdb581fcde3
Hoang <billyhoang11.ca@gmail.com>
2020-02-13 00:05:27 -0500

    test Copy

--- index.js
+++ index.js
@@ -1,97 +1,58 @@
-const express = require("express"); //this imports the express package that was installed within your application
-
-const app = express(); // this creates your express app object
-
-const exphbs= require("express-handlebars");
-
-const productModel = require("./model/product");//call javascript file that has elements inside: fakeDB, init and get()
+const express = require("express");
+const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 
-app.engine("handlebars",exphbs());
-app.set("view engine", "handlebars");
+//creation of app object
+const app = express();
 
-app.use(express.static("public"));
 
-var bodyParser = require('body-parser');
+//handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+//body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 
+//loading static assests middleware
+app.use(express.static("public"));
 
 
-//Route for the Home Page
 app.get("/",(req,res)=>{
+
     res.render("home",{
-        title:"Home Page",
-        category_list: productModel.getcategory_list(),
-        bestSelling: productModel.getbestseller_list()
+        title: "Home Page"
+    });
 
-    });
 });
-/**REQUEST FORM */
-app.get("/signin",(req,res)=>{
-    res.render("signin",{
-        title:"Login Page",
-    });
-});
-app.get("/signup",(req,res)=>{
-    res.render("signup",{
-        title:"Sign Up Page",
-    });
-});
-/**RESPOND FORM */
-app.post("/signIn_Form",(req,res)=> {
-    const error  = [];
-    //a small validation for the password, it will be upgraded in the future version
-    if (req.body.password.length < 6) {
-        error.push(' password must at least 6 characters');
-        res.render("notification",{
-            title:"Signin failed",
-            email:req.body.email,
-            error: error
-        });
-    };
-    //if no error
-    if (error.length==0){
-        res.render("notification",{
-            title:"Successfully Login with ",
-            email:req.body.email
-        });
-    };
-});
-/*sign up form*/
-app.post("/signUp_Form",(req,res)=> {
-    const error  = [];
-    //a small validation for the password, it will be upgraded in the future version
-    if (req.body.password.length < 6) {
-        error.push(' password must at least 6 characters');
-    };
-    if (req.body.password != req.body.password_again){
-        error.push(' password must match');
-        
-    }
-    //if no error
-    if (error.length==0){
-        res.render("notification",{
-            title:"Successfully Signup with ",
-            email:req.body.email
-        });
-    } else {
-        res.render("notification",{
-            title:"Signup failed",
-            email:req.body.email,
-            error: error
-        });
-    };
-});
-/**PRODUCTPAGE */
-app.get("/productspage",(req,res)=>{
 
-    res.render("products",{
-        title:"Products",
-        products :productModel.getAllProducts(),
-    });
+
+app.get("/about-us",(req,res)=>{
+
+    res.render("about",{
+        title: "Home Page"
+    })
+
 });
-const PORT=process.env.PORT || 3000;
-//This creates an Express Web Server that listens to HTTP Reuqest on port 3000
+
+app.get("/about-us",(req,res)=>{
+
+    res.render("about",{
+        title: "About Us Page"
+    })
+
+});
+
+app.get("/contact-us",(req,res)=>{
+
+    res.render("contact",{
+        title: "Contact Us page"
+    })
+
+});
+
+const PORT= process.env.PORT || 3000;
 app.listen(PORT,()=>{
-    console.log(`Web Server Started`);
-});
+    console.log(`Web Server`);
+})
